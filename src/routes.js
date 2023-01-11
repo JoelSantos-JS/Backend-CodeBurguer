42d8cda6f1d9de56945b790e1a8dbdaddfd16017
Joel Santos <joeltere9@gmail.com>
2023-01-11 17:55:07 -0300

    create model

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
 import { Router } from "express";
+import { v4 } from "uuid";
+
+import User from "./app/models/User";
+import "./database";
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "HI" });
+routes.get("/", async (req, res) => {
+  const user = await User.create({
+    id: v4(),
+    name: "Joel Santos",
+    email: "joeltere8@gmail.com",
+    password_hash: "joel123",
+    admin: true,
+  });
+  return res.json(user);
 });
 
 export default routes;
