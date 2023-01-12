ecb8ab6074453e3092cf605d08c72435a79ccbb1
Joel Santos <joeltere9@gmail.com>
2023-01-12 18:41:44 -0300

    create sessionController

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,49 @@
+// store => Cadastrar / Adicionar
+
+// index => Listar Varios
+// show  => List apenas um
+// update  => Atualizar
+// delete => deletar
+
+import { v4 } from "uuid";
+import User from "../models/User";
+import * as Yup from "yup";
+
+class UserController {
+  async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string().email().required(),
+      password: Yup.string().required().min(5),
+      admin: Yup.boolean(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res
+        .status(400)
+        .json({ error: "Tenha certerza que os seu dados estao corretos " });
+    }
+
+    const { name, email, password, admin } = req.body;
+
+    const userAlreadyExist = await User.findOne({
+      where: { email },
+    });
+
+    if (userAlreadyExist) {
+      return res.status(400).json({ error: "email already exists" });
+    }
+
+    const user = await User.create({
+      id: v4(),
+      name: name,
+      email: email,
+      password: password,
+      admin: admin,
+    });
+
+    return res.status(201).json(user);
+  }
+}
+
+export default new UserController();
--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,10 @@
 import { Router } from "express";
-import { v4 } from "uuid";
-
-import User from "./app/models/User";
-import "./database";
+import SessionController from "./app/controllers/SessionController";
+import UserController from "./app/controllers/UserController";
 
 const routes = new Router();
 
-routes.get("/", async (req, res) => {
-  const user = await User.create({
-    id: v4(),
-    name: "Joel Santos",
-    email: "joeltere8@gmail.com",
-    password_hash: "joel123",
-    admin: true,
-  });
-  return res.json(user);
-});
+routes.post("/users", UserController.store);
+routes.post("/sessions", SessionController.store);
 
 export default routes;
