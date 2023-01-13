d72ef0e9cd735b9f045a6f8fc2087693e010cc66
Joel Santos <joeltere9@gmail.com>
2023-01-13 18:48:35 -0300

    update product

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,16 @@
 import { Router } from "express";
+import ProductController from "./app/controllers/ProductController";
 import SessionController from "./app/controllers/SessionController";
 import UserController from "./app/controllers/UserController";
-
+import multer from "multer";
+import multerConfig from "./config/multer";
 const routes = new Router();
+
+const upload = multer(multerConfig);
 
 routes.post("/users", UserController.store);
 routes.post("/sessions", SessionController.store);
+routes.post("/products", upload.single("file"), ProductController.store);
+routes.get("/products", ProductController.index);
 
 export default routes;
